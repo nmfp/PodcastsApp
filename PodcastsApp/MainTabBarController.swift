2b76725e880421c4d3166e36ac61c588e156f408
Nuno Pereira <nuno-f-pereira@telecom.pt>
2018-02-26 23:18:58 +0000

    Initial commit with tabbar controller with 3 buttons.

---
+++ PodcastsApp/MainTabBarController.swift
@@ -0,0 +1,35 @@
+//
+//  MainTabBarController.swift
+//  PodcastsApp
+//
+//  Created by Nuno Pereira on 26/02/2018.
+//  Copyright © 2018 Nuno Pereira. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        UINavigationBar.appearance().prefersLargeTitles = true
+        view.backgroundColor = .white
+        
+        
+        viewControllers = [
+            generateNavigationController(with: UIViewController(), title: "Search", image: #imageLiteral(resourceName: "search")),
+            generateNavigationController(with: UIViewController(), title: "Favorites", image: #imageLiteral(resourceName: "favorites")),
+            generateNavigationController(with: UIViewController(), title: "Downloads", image: #imageLiteral(resourceName: "downloads"))
+        ]
+    }
+    
+    
+    fileprivate func generateNavigationController(with rootViewController: UIViewController, title: String, image: UIImage) -> UIViewController {
+        let navController = UINavigationController(rootViewController: rootViewController)
+        rootViewController.navigationItem.title = title
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = image
+        return navController
+    }
+}
